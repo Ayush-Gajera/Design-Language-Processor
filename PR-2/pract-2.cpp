54d3e3d71043c057b6e06522d777146903b3bdbb
Ayush-Gajera <gajera70964@gmail.com>
2024-12-30 22:15:53 +0530

    Update pract-2.cpp

--- PR-2/pract-2.cpp
+++ PR-2/pract-2.cpp
@@ -1,33 +1,6 @@
-/*
-Implement DFA as below:
-no. of input symbols: 2
-input symbol: a b
-enter number of states: 1
-initial state: 1
-number of accepting states: 1
-Accepting states: 2
-transistion table:
-1 to a->2
-1 to b->3
-2 to a->1
-2 to b->4
-3 to a->4
-3 to b->1
-4 to a->3
-4 to b->2
-
-input string : abbabab
-
-output : vaild
-Other programs:
-1. string over 0,1 where every 0 immediately by 11
-2. string over a b c start and end with same letter
-3. string over lower case alphabet and digits, starts with alphabet only.
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
-// 1: DFA for the string of types "abbabab"
 bool testCase1(const string &input) {
     int transition[5][2]={{0, 0}, {2, 3}, {1, 4}, {4, 1}, {3, 2}};
     int currentState=1;
@@ -39,7 +12,7 @@ bool testCase1(const string &input) {
     return currentState== 2;
 }
 
-// 2: DFA for strings where every '0' is followed by '11'
+
 bool testCase2(const string &input) {
     int i = 0;
     while(i < input.length()) {
@@ -58,7 +31,7 @@ bool testCase2(const string &input) {
     return true;
 }
 
-// 3: DFA for strings over 'a', 'b', 'c' that start and end with the same letter
+
 bool testCase3(const string &input) {
     int n = input.length();
     if(n == 0 || input[0] != input[n - 1]) {
@@ -72,7 +45,7 @@ bool testCase3(const string &input) {
     return true;
 }
 
-// 4: DFA for strings over lowercase alphabets and digits, starting with an alphabet
+
 bool testCase4(const string &input) {
 /*    if (!islower(input[0])) {
         return false; // Must start with a lowercase alphabet
